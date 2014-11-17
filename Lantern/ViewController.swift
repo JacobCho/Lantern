b3b3a0a2e5eb377f1b1ed0b8cfef1c7df340df7b
Jacob Cho <jacobshcho@gmail.com>
2014-11-17 12:50:30 -0800

    Initial commit

---
+++ Lantern/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lantern
+//
+//  Created by Jacob Cho on 2014-11-17.
+//  Copyright (c) 2014 J & T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

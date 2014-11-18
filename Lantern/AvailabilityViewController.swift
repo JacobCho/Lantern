5bdf350518d9d308f2bb8fc2c3dd8e62374e9e16
TuckerSherman <tsherman@outlook.com>
2014-11-17 23:04:17 -0800

    collection view configured

---
+++ Lantern/AvailabilityViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AvailabilityViewController.swift
+//  Lantern
+//
+//  Created by Tucker Sherman on 11/17/14.
+//  Copyright (c) 2014 J & T. All rights reserved.
+//
+
+import UIKit
+
+class AvailabilityViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = UIColor.whiteColor()
+        
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("personCell", forIndexPath: indexPath) as PersonCell
+        cell.imageView.image = UIImage(named: "person")
+        cell.nameLabel.text = "Name"
+        
+        return cell
+    }
+
+}
---
+++ Lantern/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Lantern
+//
+//  Created by Tucker Sherman on 11/17/14.
+//  Copyright (c) 2014 J & T. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    
+}

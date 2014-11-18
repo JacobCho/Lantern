cdd0285214f95828a675b56d304651678eabef08
TuckerSherman <tsherman@outlook.com>
2014-11-17 22:35:40 -0800

    views added to storyboard

---
+++ AvailabilityCollectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  AvailabilityCollectionViewController.h
+//  Lantern
+//
+//  Created by Tucker Sherman on 11/17/14.
+//  Copyright (c) 2014 J & T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PersonCell.h"
+
+@interface AvailabilityCollectionViewController : UICollectionViewController
+
+@property (strong,nonatomic) NSArray* users;
+
+
+@end

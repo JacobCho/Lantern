4db744b3d69773686f423fa7c7788e0cd897acea
Jacob Cho <jacobshcho@gmail.com>
2014-11-18 12:16:47 -0800

    Fixed login

--- Lantern/LoginViewController.swift
+++ Lantern/LoginViewController.swift
@@ -29,10 +29,13 @@ class LoginViewController: UIViewController, UITextFieldDelegate
         super.viewDidAppear(animated)
         
         // If user is logged in, go to collection view
-        var user = User.currentUser()
-        if (user.username != nil) {
-            self.performSegueWithIdentifier("loginSegue", sender: self)
+        let user = User.currentUser()
+        if user != nil {
+            if user.username != nil {
+                self.performSegueWithIdentifier("loginSegue", sender: self)
+            }
         }
+
     }
     
     

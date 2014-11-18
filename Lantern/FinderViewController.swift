38dea6d4231340222d1f3a4841c051161dc3d65a
TuckerSherman <tsherman@outlook.com>
2014-11-18 12:42:02 -0800

    ready to pull

--- Lantern/FinderViewController.swift
+++ Lantern/FinderViewController.swift
@@ -26,6 +26,10 @@ class FinderViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        
+        
+        
         // Dispose of any resources that can be recreated.
     }
 

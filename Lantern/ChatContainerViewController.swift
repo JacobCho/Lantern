ddab330b6a6be0d516ec1a789ec2184b469a1348
TuckerSherman <tsherman@outlook.com>
2014-11-20 19:45:25 -0800

    keyboard not hiding text field

--- Lantern/ChatContainerViewController.swift
+++ Lantern/ChatContainerViewController.swift
@@ -10,27 +10,62 @@ import UIKit
 
 class ChatContainerViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var chatTextField: UITextField!
+    
+    @IBOutlet var containerView: UIView!
+    
     var messageRecipient: User!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.chatTextField.delegate = self
-        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "viewSlide:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "viewSlide:", name: UIKeyboardWillHideNotification, object: nil)
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
     }
-    
     /* UITextFieldDelegate Method */
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+    
+    @IBOutlet var constraintToBottom: NSLayoutConstraint!
+    
+    
+    func viewSlide(notification:NSNotification) {
+        
+        
+        
+        println("we got a notification")
+        if let keyboardInfo:Dictionary = notification.userInfo {
+            if notification.name == UIKeyboardWillShowNotification {
+                
+                
+                UIView.animateWithDuration(1, animations: { () -> Void in
+                    self.constraintToBottom.constant += (keyboardInfo[UIKeyboardFrameBeginUserInfoKey] as NSValue).CGRectValue().height
+                    self.view.layoutIfNeeded()
+                    return
+                })
+                
+            } else if notification.name == UIKeyboardWillHideNotification {
+                UIView.animateWithDuration(1, animations: { () -> Void in
+                    self.constraintToBottom.constant -= (keyboardInfo[UIKeyboardFrameBeginUserInfoKey] as NSValue).CGRectValue().height
+                    self.view.layoutIfNeeded()
 
+                    return
+                })
+            }
+//        UIView.commitAnimations()
+        }
+    }
+    
     @IBAction func cancelButtonPressed(sender: UIButton) {
         self.dismissViewControllerAnimated(true, completion: nil)
+        self.chatTextField.resignFirstResponder()
+
     }
     @IBAction func sendButtonPressed(sender: UIButton) {
         var currentUser = User.currentUser()
